4f2f8753a5bb9865a7d0aad4573c62d898acddb8
Igor <IggorsBot@gmail.com>
2020-02-23 20:43:27 +0300

    add Chats component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import Menu from './Menu/Menu'
+import Chats from './Chats'
 
 class App extends Component {
   render() {
@@ -10,7 +11,9 @@ class App extends Component {
         <div className="p-2 bd-highlight px-5 border-right">
           <Menu />
         </div>
-        <div className="p-2 bd-highlight">Flex item</div>
+        <div className="p-2 bd-highlight border-right">
+          <Chats />
+        </div>
         <div className="p-2 bd-highlight flex-grow-1">Third flex item</div>
       </div>
     )
--- frontend/src/components/Chats.js
+++ frontend/src/components/Chats.js
@@ -5,7 +5,39 @@ import ReactDOM from 'react-dom';
 class Chats extends Component {
   render() {
     return(
+        <div>
+          <div className="row mt-3">
+            <div className="mr-5"><h2 className="mx-5 my-2">Chats</h2></div>
+            <div className="border rounded"><img className="mt-3 mx-3" src="http://localhost:800/static/icons/avatar.svg" alt="user" height="18" width="18"/></div>
+            <div className="border rounded mx-2 mr-5"><img className="mt-3 mx-3" src="http://localhost:800/static/icons/add-user.png" alt="user" height="18" width="18"/></div>
+          </div>
 
+          <div className="form-group my-5 mx-2">
+            <input className="form-control input-lg" id="inputlg" type="text" placeholder="Search chats"/>
+          </div>
+
+          <div className="border-bottom">
+            <div className="row ml-2">
+              <div className=" my-2 col-2">
+                <img src="http://localhost:800/static/icons/folder.png" alt="folder" height="24" width="24"/>
+              </div>
+
+              <div className="col">
+                <div className="row">Townsend Seary</div>
+                <div className="row">What's up, how are you?</div>
+              </div>
+
+              <div className="col-2">
+                <div className="row"></div>
+                <div className="row">11:05</div>
+              </div>
+
+
+            </div>
+          </div>
+
+
+        </div>
     )
 
   }
