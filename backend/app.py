9c59367933fb5178fe58bffb2d2219ab6b1617ec
Igor <IggorsBot@gmail.com>
2020-02-27 15:46:32 +0300

    get message from websocket

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,37 @@
-from aiohttp import web
+from aiohttp import web, WSMsgType
+
 
 async def index(request):
     return web.FileResponse('./../frontend/templates/index.html')
 
+
+async def websocket_handler(request):
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    print('here')
+
+    async for msg in ws:
+        print("message", msg.data)
+        if msg.type == WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.send_str(msg.data)
+        elif msg.type == WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+
+    return ws
+
+
 app = web.Application()
 app.add_routes([web.get('/', index)])
+app.add_routes([web.get('/ws', websocket_handler)])
+
+
+
+
 web.run_app(app)
