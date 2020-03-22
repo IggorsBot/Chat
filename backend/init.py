9793b3d7342022652b0123176ee85ac96ed5e502
IggorsBot <IggorsBot@gmail.com>
2020-03-22 13:11:42 +0300

    create test table

--- backend/init.py
+++ backend/init.py
@@ -1,6 +1,30 @@
+import aiopg
+import asyncio
 
-async def create_table():
-    pass
+dsn = 'dbname=chatdb user=chat password=chatpass host=localhost'
 
-async def init_database():
-    pass
+
+async def create_tables():
+    print("-----------------------------")
+    print("Database initialized...")
+    await create_chat_table()
+    print("Database initialized finished")
+    print("-----------------------------")
+
+
+async def create_chat_table():
+    async with aiopg.create_pool(dsn) as pool:
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                s = """
+                CREATE TABLE IF NOT EXISTS chat(
+                    chat_id integer,
+                    name varchar(40),
+                    user_id integer
+                )
+                """
+                await cur.execute(s)
+
+def init_database():
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(create_tables())
